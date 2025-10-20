639b790068615f8155606ff8ae6e89b118761eed
jiamu <jiamu.chai@proton.me>
2025-10-20 16:09:21 -0500

    clean up determinism module

--- rocq/Determinism.v
+++ rocq/Determinism.v
@@ -7,14 +7,11 @@ Import ListNotations.
 Module Type DeterminismDef (LD : LangDefs).
     Import LD.
     Import LangNotations.
-    Parameter lang : LanguageSemantics.
-    #[global] Instance lang_inst : LanguageSemantics.
-        exact lang.
-    Defined.
-    
     Section Determinism.
+        (* take this one as axiom *)
         Definition one_step_det cs :=
             forall cs1 cs2 a1 a2,
                 cs -->[a1] cs1 -> cs -->[a2] cs2 -> (a1 = a2) /\ (cs1 = cs2).
+        (* TODO: in addition to one step, prove that 2 finite traces produced from cs must be equal*)
     End Determinism.
 End DeterminismDef.
